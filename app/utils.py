64a30d1c0f2c593450bb92ebce1197a9c4d615cc
Sanaullah <76056239+bwithai@users.noreply.github.com>
2022-06-15 16:58:19 +0500

    Update utils.py

--- app/utils.py
+++ app/utils.py
@@ -8,7 +8,6 @@ settings: Settings = get_env()
 def get_database_url():
     # url = "sqlite:///database.sqlite"
     url = "mysql+mysqldb://bwithai:sana123@localhost:3306/notifications"
-    # URI = mysql+mysqldb: // syed: syedfurqan @ localhost:3306 / mobile_accessories
     # db_password = urllib.parse.quote_plus(settings.db_pass)
     # url = f"postgresql://{settings.db_user}:{db_password}@{settings.db_host}:{settings.db_port}/{settings.db_name}"
     return url
